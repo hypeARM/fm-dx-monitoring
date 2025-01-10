0f52524e1e6e3f63410574ef0d237c9ac792ad43
Marek Farkaš <ardoniceksvk@gmail.com>
2025-01-10 13:14:03 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,145 @@
+const axios = require('axios');
+const puppeteer = require('puppeteer');
+const path = require('path');
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+const webserverLink = "http://192.168.1.15:8080";
+
+let ipData = null;
+let frequencyData = [];
+let localStorageInfo = [];
+const dataFilePath = path.join(__dirname, 'frequencyData.json');
+
+const europe_programmes = [
+    "No PTY", "News", "Current Affairs", "Info",
+    "Sport", "Education", "Drama", "Culture", "Science", "Varied",
+    "Pop Music", "Rock Music", "Easy Listening", "Light Classical",
+    "Serious Classical", "Other Music", "Weather", "Finance",
+    "Children's Programmes", "Social Affairs", "Religion", "Phone-in",
+    "Travel", "Leisure", "Jazz Music", "Country Music", "National Music",
+    "Oldies Music", "Folk Music", "Documentary", "Alarm Test"
+];
+
+console.log(`
+    \x1b[32m.  .        ,              
+    \x1b[34m|\/| _ ._ *-+- _ ._.*._  _ 
+    \x1b[33m|  |(_)[ )| | (_)[  |[ )(_]
+    \x1b[35m                        ._|
+    \x1b[0mby Noobish @ FMDX.org
+    `);
+    
+
+try {
+    if (fs.existsSync(dataFilePath)) {
+        const data = fs.readFileSync(dataFilePath, 'utf8');
+        frequencyData = JSON.parse(data);
+        console.log('[INFO] Loaded frequency data from file:', dataFilePath);
+    }
+} catch (error) {
+    console.error('Error reading frequency data file:', error);
+}
+
+async function fetchIPData() {
+    try {
+        const response = await axios.get(webserverLink + '/api');
+        ipData = response.data;
+        const pty = ipData.pty;
+        const ptyString = europe_programmes[pty] || 'Unknown';
+        
+        const currentDate = new Date().toISOString();
+        
+        if (ipData.rds !== true && ipData.ps === "") {
+            const roundedFreq = parseFloat(ipData.freq).toFixed(1);
+            frequencyData.push({ freq: roundedFreq, sig: ipData.sig, pty: ptyString, date: currentDate });
+            return;
+        }
+        
+        const roundedFreq = parseFloat(ipData.freq).toFixed(1);
+        const existingFreqIndex = frequencyData.findIndex(item => item.freq === roundedFreq);
+        
+        if (existingFreqIndex !== -1) {
+            frequencyData[existingFreqIndex] = { ...ipData, freq: roundedFreq, pty: ptyString, date: currentDate };
+        } else {
+            frequencyData.push({ ...ipData, freq: roundedFreq, pty: ptyString, date: currentDate });
+        }
+        
+        frequencyData.sort((a, b) => parseFloat(a.freq) - parseFloat(b.freq));
+    } catch (error) {
+        console.error('Error fetching IP data:', error);
+    }
+}
+
+setInterval(fetchIPData, 5000);
+setInterval(() => {
+    fs.writeFile(dataFilePath, JSON.stringify(frequencyData, null, 2), (err) => {
+        if (err) {
+            console.error('Error saving frequency data:', err);
+        } else {
+            console.log('[INFO] Frequency data saved successfully.');
+        }
+    });
+}, 5 * 60 * 1000);
+
+async function fetchLocalStorageData() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    
+    await page.goto(webserverLink);
+    
+    const localStorageData = await page.evaluate(() => {
+        let data = {};
+        
+        for (let key in localStorage) {
+            data[key] = localStorage.getItem(key);
+        }
+        
+        const tunerNameElement = document.querySelector('#tuner-name');
+        let tunerName = tunerNameElement ? tunerNameElement.textContent : null;
+        
+        if (tunerName) {
+            tunerName = tunerName.trim();
+            const outerBracketsRemoved = tunerName.replace(/^\[(.*)\]$/, '$1').trim();
+            tunerName = outerBracketsRemoved;
+        }
+        data['tunerName'] = tunerName;
+        delete data['sd0'];
+        delete data['sd1'];
+        delete data['sd'];
+        delete data['ad'];
+        
+        return data;
+    });
+    
+    //console.log('LocalStorage data:', localStorageData);
+    
+    await browser.close();
+    return localStorageData;
+}
+
+fetchLocalStorageData().then(data => {
+    localStorageInfo = data;
+}).catch(error => {
+    console.error('Error fetching localStorage data:', error);
+});
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '../web')));
+app.set('views', path.join(__dirname, '../web'));
+
+const endpoints = express.Router();
+endpoints.get('/', (req, res) => {
+    res.render('index', { data: frequencyData, info: localStorageInfo });
+});
+
+endpoints.get('/data', (req, res) => {
+    res.json(frequencyData);
+});
+
+app.use('/', endpoints);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`[INFO] Server is running on http://localhost:${PORT}`);
+});
---
+++ web/js/signalchart.js
@@ -0,0 +1,156 @@
+fetch('./data')
+.then(response => response.json())
+.then(data => {
+  const frequencies = data.map(item => parseFloat(item.freq));
+  const sigValues = data.map(item => parseFloat(item.sig));
+  const psValues = data.map(item => item.ps);
+  const piValues = data.map(item => item.pi);
+  
+  const allFrequencies = [];
+  for (let freq = 87.5; freq <= 108.0; freq += 0.1) {
+    allFrequencies.push(parseFloat(freq.toFixed(1)));
+  }
+  
+  const sigValuesWithZeros = allFrequencies.map(freq => {
+    const closestIndex = frequencies.findIndex(f => Math.abs(f - freq) < 0.05);
+    return closestIndex !== -1 ? sigValues[closestIndex] : 0;
+  });
+  
+  const annotations = [];
+  allFrequencies.forEach((freq, index) => {
+    const closestIndex = frequencies.findIndex(f => Math.abs(f - freq) < 0.05);
+    if (closestIndex !== -1 && sigValuesWithZeros[index] !== 0 && typeof psValues[closestIndex] !== "undefined" && psValues[closestIndex].length > 0) {
+      annotations.push({
+        type: 'label',
+        xValue: freq,
+        yValue: sigValuesWithZeros[index] + 10,
+        color: '#ccc',
+        content: `${freq.toFixed(1)} ${psValues[closestIndex]}`,
+        font: {
+          size: 12,
+          family: 'Titillium Web'
+        },
+        rotation: 270,
+        position: 'right',
+        yAdjust: -25,
+        animation: false,
+      });
+    }
+  });
+  
+  const pointData = allFrequencies.map((freq, index) => {
+    const closestIndex = frequencies.findIndex(f => Math.abs(f - freq) < 0.05);
+    const ps = closestIndex !== -1 ? psValues[closestIndex] : null;
+    const pi = closestIndex !== -1 ? piValues[closestIndex] : null;
+    
+    return {
+      x: freq,
+      y: sigValuesWithZeros[index],
+      ps: ps,
+      pi: pi,
+    };
+  });
+  
+  const ctx = document.getElementById('signal-chart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      datasets: [{
+        label: 'Signal Strength (dBf)',
+        data: pointData,
+        fill: true,
+        backgroundColor: 'rgba(88, 219, 171, 0.05)',
+        borderColor: 'rgb(88, 219, 171)',
+        tension: 0.3,
+        pointRadius: function(context) {
+          const dataPoint = context.raw;
+          return (dataPoint.y === 0 || (!dataPoint.ps || !dataPoint.pi)) ? 0 : 5;
+        },
+        pointHoverRadius: 8,
+        pointHitRadius: 10,
+        spanGaps: true,
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      transitions: {
+        zoom: {
+          animation: {
+            duration: 0
+          }
+        }
+      },
+      plugins: {
+        legend: {
+          display: false
+        },
+        tooltip: {
+          enabled: true, 
+          mode: 'nearest', 
+          intersect: false,
+          position: 'nearest',
+          callbacks: {
+            label: function(tooltipItem) {
+              const dataPoint = tooltipItem.raw;
+              const ps = dataPoint.ps || 'N/A';
+              const pi = dataPoint.pi || 'N/A';
+              const freq = dataPoint.x.toFixed(1);  // Format the frequency value (x value) with one decimal point
+              return `${dataPoint.y.toFixed(0)} dBf • ${ps} • ${pi} • ${freq} MHz`;
+            }
+          }
+        },
+        annotation: {
+          annotations: annotations,
+          animation: false
+        },
+        zoom: {
+          pan: {
+            enabled: true,
+            mode: 'x',
+            speed: 5,
+          },
+          limits: {
+            x: { min: 87.3, max: 108.2 },
+          },
+          zoom: {
+            wheel: {
+              enabled: true,
+            },
+            pinch: {
+              enabled: true
+            },
+            mode: 'x',
+          },
+        }
+      },
+      scales: {
+        x: {
+          type: 'linear',
+          min: 87.3,
+          max: 108.0,
+          ticks: {
+            stepSize: 0.1,
+            maxTicksLimit: 25,
+            callback: function(value) {
+              return value.toFixed(1);  // Format tick values with one decimal
+            }
+          },
+          title: {
+            display: true,
+            text: 'Frequency (MHz)'
+          }
+        },
+        y: {
+          min: 0,
+          max: 130,
+          title: {
+            display: true,
+            text: 'Signal Strength (dBf)'
+          }
+        }
+      }
+    }
+  });
+})
+.catch(error => console.error('Error fetching data:', error));
